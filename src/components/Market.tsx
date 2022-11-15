2ef18e63b4558d7ce4cc6e81c4eaa4511a2e163a
Aleksey Vyatkin <avyatkin@gmail.com>
2022-11-15 23:35:16 +0300

    Market table improved

--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -39,6 +39,7 @@ const row = (order: Order, cn: string, key: number) => <div className={classes.r
 const Market = (props: MarketProps) => {
     const divRef = React.useRef<HTMLDivElement | null>(null);
     const [titleWidth, setTitleWidth] = React.useState<number>();
+
     React.useEffect(() => {
         if (divRef.current) {
             setTitleWidth(divRef.current.clientWidth - 20/*padding-right*/);
@@ -50,11 +51,19 @@ const Market = (props: MarketProps) => {
     const spread = props.ask_orders[props.ask_orders.length - 1]?.price - props.bid_orders[0]?.price;
     const pspread = spread / props.bid_orders[0]?.price * 100;
 
+    let top, bottom;
+    if(props.ask_orders.length > props.bid_orders.length) {
+        bottom = <div style={{height: (props.ask_orders.length-props.bid_orders.length)*25/*.row height*/}}></div>;
+    } else if(props.ask_orders.length < props.bid_orders.length) {
+        top = <div style={{height: (props.bid_orders.length-props.ask_orders.length)*25/*.row height*/}}></div>;
+    }
+
     return <div role='table'>
         <div className={classes.row + ' ' + classes.title} style={{ width: titleWidth }} role='rowheader'>
             <HeadCell>Price</HeadCell><HeadCell>Size</HeadCell><HeadCell>Total</HeadCell>
         </div>
         <div className={classes.container} ref={divRef} role='rowgroup'>
+            {top}
             {props.ask_orders.map((order, i) => row(order, classes.ask, i))}
             {!!props.bid_orders.length && !!props.ask_orders.length &&
                 <div className={classes.row + ' ' + classes.spread} role='rowheader'>
@@ -63,6 +72,7 @@ const Market = (props: MarketProps) => {
                     <Cell>{pspread.toFixed(2)}%</Cell>
                 </div>}
             {props.bid_orders.map((order, i) => row(order, classes.bid, i))}
+            {bottom}
         </div>
     </div>
 }
